4f54e7b13ab6692dd35170d7a61469b8cb191c7a
William Elí O.L <ramun346416@gmail.com>
2023-01-01 09:38:12 +0000

    Add files via upload

---
+++ round#1_Ej#26.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main ()
+{
+
+    int a;
+
+    printf("\n Dame el numero \n");
+    scanf("%d", &a);
+    printf("\t En hexadecimal es: %x\t", a);
+    printf("\t En binario es: %b\t", a);
+    printf("\n");
+}
